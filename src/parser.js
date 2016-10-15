5d9ac09cd4492e89b8b0753a3356d5679bb5c121
Joel Wallis Jucá <joelwallis@gmail.com>
2016-10-15 20:12:45 -0300

    Bluebird instead of Q as promise library
    Also Parser.parse is now rejecting promise instead of throwing errors
    
    Fixes #1
    Fixes #7

--- src/parser.js
+++ src/parser.js
@@ -1,35 +1,23 @@
-import q from 'q'
-import http from 'q-io/http'
+import Promise from 'bluebird'
+import requestAsync from 'request-promise'
 import validator from 'validator'
 import {default as podcastParser} from 'node-podcast-parser'
 
 class Parser {
 
-  static process(response) {
-
-    const deferred = q.defer()
-    podcastParser(response, (err, data) => {
-      if (err) {
-        deferred.reject(new Error(err))
-      } else
-        deferred.resolve(data)
-    })
-    return deferred.promise
-
+  static process(xml) {
+    return Promise.promisify(podcastParser)(xml)
   }
 
   static parse(url) {
 
-    if(!validator.isURL(url))
-      throw new Error('Invalid Podcast URL')
-
-    return http.read({
-      url: url,
-      method: 'GET'
-    }).then((response) => {
-      return response.toString()
-    }).then(Parser.process)
-
+    return new Promise((res, rej) => {
+      if (!validator.isURL(url)) throw new Error('Invalid Podcast URL')
+      requestAsync(url)
+        .then(Parser.process)
+        .then(res)
+        .catch(rej)
+    })
   }
 
 }
